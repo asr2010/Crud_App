ea89d22497920fe2ca089a8ed658b4d9cb47deb0
arpit raj <arpitshekharraj@gmail.com>
2023-02-21 20:57:47 -0500

    Cloud Formation Template

---
+++ vitacom/infrastructure/deploy.sh
@@ -0,0 +1,1 @@
+aws cloudformation create-stack --stack-name health-vitals --template-body file://cf.template
---
+++ vitacom/infrastructure/update.sh
@@ -0,0 +1,1 @@
+aws cloudformation update-stack --stack-name health-vitals --template-body file://cf.template
